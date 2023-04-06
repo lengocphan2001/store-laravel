8237367bf32e56d543d7e7fece89959d600faff7
Tuyên Nguyễn <tts-tuyennd@amaisoft.com>
2023-04-06 11:46:00 +0700

    fix id

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -6,6 +6,7 @@ use App\Helpers\UserHelper;
 use App\Http\Controllers\Controller;
 use App\Services\User\OrderService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -15,10 +16,10 @@ class AuthController extends Controller
         // return view('admin.login');
     }
 
-    public function detail ($id)
+    public function detail ()
     {
         $data['title'] = UserHelper::getPageTitle(trans('user.detail.account'));
-        $data['orders'] = OrderService::getInstance()->getListOrders($id);
+        $data['orders'] = OrderService::getInstance()->getListOrders(auth()->guard('web')->id());
 
         return view('user.info.index')->with(['data' => $data]);
     }
